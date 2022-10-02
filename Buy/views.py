c94a85d4d08d660dbef14226e5f7e202e7b2b5ee
manzi osee <oseemanzi3@gmail.com>
2022-10-02 16:07:28 +0200

    commit #2

--- Buy/views.py
+++ Buy/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BuySerializer
+from .models import BuyModel
 
 # Create your views here.
+
+class BuyProduct(generics.CreateAPIView):
+    queryset=BuyModel.objects.all()
+    
